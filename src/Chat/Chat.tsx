4eeb35f145277bfc61bc11ef7a9cfdeb016268a2
Anna <anna.zhukanna@gmail.com>
2022-12-06 18:01:42 +0500

    add basic chat structure

--- src/Chat/Chat.tsx
+++ src/Chat/Chat.tsx
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 
 function Chat() {
-  return <div className='font-bold accent-red-900'>Привет!</div>;
+  const userName = sessionStorage.getItem('username');
+  const [styles, setStyle] = useState('font-bold absolute top-1/2');
+  const [message, setMessage] = useState('');
+  // const [history, setHistory] = useState(() => {
+  //   const saved = localStorage.getItem('history');
+  //   return saved ? JSON.parse(saved) : '';
+  // });
+  const history = [
+    {
+      sender: 'John',
+      message: 'Hello',
+      date: '2022-12-06T12:39:12.255Z',
+    },
+    {
+      sender: userName,
+      message: 'Hello, friend!',
+      date: '2022-12-06T12:40:56.255Z',
+    },
+  ];
+
+  useEffect(() => {
+    const t = setTimeout(() => {
+      setStyle('font-bold text-blue-900 absolute top-0');
+    }, 2500);
+    return () => clearTimeout(t);
+  }, []);
+
+  const sendMessage = (evt: FormEvent) => {
+    evt.preventDefault();
+    localStorage.setItem('message', message);
+  };
+
+  return (
+    <div className='flex flex-col justify-end h-90 min-w-80 relative gap-6'>
+      <div className={styles}>Привет, {userName}!</div>
+      {history.map((el, i) => {
+        return (
+          <div
+            key={i}
+            className={
+              'rounded p-4' +
+              (el.sender == userName
+                ? ' bg-blue-400 self-end'
+                : ' bg-blue-200 self-start')
+            }>
+            <p className='font-bold'>{el.sender}</p>
+            <p>{el.message}</p>
+            <p>{new Date(el.date).toLocaleTimeString()}</p>
+          </div>
+        );
+      })}
+      <form
+        className='flex items-center gap-4'
+        onSubmit={(e) => sendMessage(e)}>
+        <input
+          type='text'
+          id='message'
+          className='form-input px-4 rounded-full'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          minLength={3}
+          placeholder='Введите сообщение здесь...'
+        />
+        <button
+          type='submit'
+          className='w-full bg-blue-800 hover:bg-blue-900 text-white py-2 px-4 rounded-full'
+          onClick={(e) => sendMessage(e)}>
+          Отправить
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Chat;
