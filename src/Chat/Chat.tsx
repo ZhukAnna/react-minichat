da2d44c58fb73fc3eb2648ef7e97c7c9a8825fc8
Anna Zhukova <anna.zhukanna@gmail.com>
2022-12-06 21:11:15 +0500

    implement chat UI

--- src/Chat/Chat.tsx
+++ src/Chat/Chat.tsx
@@ -1,4 +1,6 @@
 import React, { FormEvent, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Chat.css';
 
 interface Message {
   sender: string;
@@ -8,7 +10,7 @@ interface Message {
 
 function Chat() {
   const userName = sessionStorage.getItem('username');
-  const [styles, setStyle] = useState('font-bold absolute top-1/2');
+  const navigate = useNavigate();
   const [message, setMessage] = useState('');
   const [history, setHistory] = useState(() => {
     const saved = localStorage.getItem('history');
@@ -16,65 +18,86 @@ function Chat() {
       ? JSON.parse(saved)
       : [
           {
-            sender: 'John',
-            message: 'Hello',
+            sender: 'Незнакомец',
+            message: 'Привет!',
             date: '2022-12-06T12:39:12.255Z',
-          },
-          {
-            sender: userName,
-            message: 'Hello, friend!',
-            date: '2022-12-06T12:40:56.255Z',
           },
         ];
   });
-  useEffect(() => {
-    window.addEventListener('storage', (event) => {
-      if (event.newValue) setHistory(JSON.parse(event.newValue));
-    });
+
+  const scrollToBottom = () => {
     const t = setTimeout(() => {
-      setStyle('font-bold text-blue-900 absolute top-0');
-    }, 2500);
-    return () => clearTimeout(t);
+      const chat = document.querySelector('.chat');
+      if (chat)
+        chat.scrollTo({
+          top: chat.scrollHeight,
+          behavior: 'smooth',
+        });
+      clearTimeout(t);
+    }, 1200);
+  };
+
+  useEffect(() => {
+    if (!userName) {
+      navigate('/');
+    }
+    window.addEventListener('storage', (event) => {
+      if (event.newValue) {
+        setHistory(JSON.parse(event.newValue));
+        scrollToBottom();
+      }
+    });
   }, []);
 
   const sendMessage = (evt: FormEvent) => {
     evt.preventDefault();
-    const newMsg = {
-      sender: userName,
-      message,
-      date: new Date(),
-    };
-    setHistory(history.concat(newMsg));
-    localStorage.setItem('history', JSON.stringify(history.concat(newMsg)));
-    setMessage('');
+    if (message.trim().length) {
+      const newMsg = {
+        sender: userName,
+        message,
+        date: new Date(),
+      };
+      localStorage.setItem('history', JSON.stringify(history.concat(newMsg)));
+      setHistory(history.concat(newMsg));
+      scrollToBottom();
+      setMessage('');
+    }
   };
 
   return (
-    <div className='flex flex-col justify-end h-90 min-w-80 relative gap-6'>
-      <div className={styles}>Привет, {userName}!</div>
-      {history.map((el: Message, i: number) => {
-        return (
-          <div
-            key={i}
-            className={
-              'rounded p-4' +
-              (el.sender === userName
-                ? ' bg-blue-400 self-end'
-                : ' bg-blue-200 self-start')
-            }>
-            <p className='font-bold'>{el.sender}</p>
-            <p>{el.message}</p>
-            <p>{new Date(el.date).toLocaleTimeString()}</p>
-          </div>
-        );
-      })}
+    <div className='grow'>
+      <div className='font-bold text-xl text-blue-900 p-4'>
+        Привет, {userName}!
+      </div>
+      <div className='chat mb-4'>
+        <div className='flex flex-col justify-end relative gap-6 pb-6'>
+          {history.map((el: Message, i: number) => {
+            return (
+              <div
+                key={i}
+                className={
+                  'message rounded-lg' +
+                  (el.sender === userName
+                    ? ' bg-blue-300 self-end rounded-br-none'
+                    : ' bg-blue-100 self-start rounded-bl-none')
+                }>
+                <p className='font-bold'>{el.sender}</p>
+                <p>{el.message}</p>
+                <p className='text-xs text-right text-gray-500'>
+                  {new Date(el.date).toLocaleTimeString()}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
       <form
-        className='flex items-center gap-4'
+        className='flex flex-wrap sm:flex-nowrap items-center gap-4 pb-3'
         onSubmit={(e) => sendMessage(e)}>
         <input
           type='text'
           id='message'
-          className='form-input px-4 rounded-full'
+          className='form-input px-4 rounded-full w-full'
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           minLength={3}
@@ -82,7 +105,7 @@ function Chat() {
         />
         <button
           type='submit'
-          className='w-full bg-blue-800 hover:bg-blue-900 text-white py-2 px-4 rounded-full'
+          className='w-full sm:max-w-xs bg-blue-800 hover:bg-blue-900 text-white py-2 px-4 rounded-full'
           onClick={(e) => sendMessage(e)}>
           Отправить
         </button>
